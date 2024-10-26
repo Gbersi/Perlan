bde2ee94edbda7bdbdc7956ae7b6e274d6140ef0
ZhivkoDimov <97346075+ZhivkoDimov@users.noreply.github.com>
2024-10-26 11:42:59 +0000

    deck of cards

--- lib/functions/deck_of_cards.dart
+++ lib/functions/deck_of_cards.dart
@@ -9,7 +9,21 @@ Make this function return the List of integers.
  */
 
 List<int> DeckOfCards(){
-  return [] ;
-}
+    List<int> cardvalue = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+  List<int> deck = [];
+
+  for (int i = 1; i <= cardvalue.length; i++) {
+    int count = 0;
+    int tempcard = i;
+    if (count <= 4) {
+      for (int i = 0; i < 4; i++) {
+        count++;
+        deck.add(tempcard);
+      }
+    }}
+  
+    return deck;
+  }
+
 
 
