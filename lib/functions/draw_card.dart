1f6a9f2d724ffa244c143d4a33e3b673ed347340
ZhivkoDimov <97346075+ZhivkoDimov@users.noreply.github.com>
2024-10-26 14:21:59 +0000

    fiz draw and namer

--- lib/functions/draw_card.dart
+++ lib/functions/draw_card.dart
@@ -1,9 +1,36 @@
-import 'dart:math';
+import 'deal_card.dart';
 
 /*
+Create a function called DrawCard. This function should accept TWO lists
+of integers as an input: hand, deck
 
-void DrawCard(List<int> hand, List<int> deck){
-  int draw = deck
-  hand.add()
+It should deal a card from the deck to the hand.
+
+Example
+List<int> deck = [1, 2, 3, 4, 5, 6];
+List<int> hand= [];
+DrawCard(hand, deck );
+
+print(deck); // Should print [2, 3, 4, 5, 6]
+print(hand); // Should print [1]
+
+ */
+
+import 'deal_card.dart';
+import 'deck_of_cards.dart';
+
+void DrawCard(List<int> hand, List<int> deck) {
+  if (deck.isNotEmpty) {
+    hand.add(deck.removeAt(0));
+  }
 }
-*/
+
+void main() {
+  List<int> deck = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13];
+  List<int> hand = [];
+
+  DrawCard(hand, deck);
+
+  print(deck); // Should print [2, 3, 4, 5, 6]
+  print(hand); // Should print [1]
+}
