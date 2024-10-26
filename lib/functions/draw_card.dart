1055dfd4c903af381f80fdf11f6776341e083c09
Gbersi <gunnarbersi@gmail.com>
2024-10-26 12:56:42 +0000

    Merge branch 'master' of C:\Users\gunna\OneDrive\Desktop\NTV Forritunarnám\NTV heimatölva\Perlan with conflicts.

--- lib/functions/draw_card.dart
+++ lib/functions/draw_card.dart
@@ -1,6 +1,4 @@
 import 'deal_card.dart';
-import 'deck_of_cards.dart';
-
 void DrawCard(List<int> hand, List<int> deck) {
   if (deck.isNotEmpty) {
     hand.add(deck.removeAt(0));
@@ -8,11 +6,16 @@ void DrawCard(List<int> hand, List<int> deck) {
 }
 
 void main() {
-  List<int> deck = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13];
+  List<int> deck = [1, 2, 3, 4, 5, 6];
   List<int> hand = [];
 
   DrawCard(hand, deck);
 
   print(deck); // Should print [2, 3, 4, 5, 6]
   print(hand); // Should print [1]
+}
+
+
+void DrawCard(List<int> hand, List<int> deck){
+
 }
