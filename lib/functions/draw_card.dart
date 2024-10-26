48e568475fbd86a99fd5fb7ca9f906bcd8baf560
Gbersi <gunnarbersi@gmail.com>
2024-10-26 10:44:06 +0000

    update

--- lib/functions/draw_card.dart
+++ lib/functions/draw_card.dart
@@ -1,20 +1,20 @@
 import 'deal_card.dart';
+void DrawCard(List<int> hand, List<int> deck) {
+  if (deck.isNotEmpty) {
+    hand.add(deck.removeAt(0));
+  }
+}
 
-/*
-Create a function called DrawCard. This function should accept TWO lists
-of integers as an input: hand, deck
+void main() {
+  List<int> deck = [1, 2, 3, 4, 5, 6];
+  List<int> hand = [];
 
-It should deal a card from the deck to the hand.
+  DrawCard(hand, deck);
 
-Example
-List<int> deck = [1, 2, 3, 4, 5, 6];
-List<int> hand= [];
-DrawCard(hand, deck );
+  print(deck); // Should print [2, 3, 4, 5, 6]
+  print(hand); // Should print [1]
+}
 
-print(deck); // Should print [2, 3, 4, 5, 6]
-print(hand); // Should print [1]
-
- */
 
 void DrawCard(List<int> hand, List<int> deck){
 
